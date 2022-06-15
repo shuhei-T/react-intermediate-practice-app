7a437ae0ae407bd37ed3455fe01377f52767f7b1
shuhei-T <nofuture742617000027@gmail.com>
2022-06-15 18:49:16 +0900

    ヘッダーの作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,8 +1,9 @@
-import { memo, VFC } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { Login } from '../components/pages/Login'
-import { homeRoutes } from './HomeRoutes'
-import { Page404 } from '../components/pages/Page404'
+import { memo, VFC } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Login } from '../components/pages/Login';
+import { homeRoutes } from './HomeRoutes';
+import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/template/HeaderLayout';
 
 export const Router: VFC = memo(() => {
   return (
@@ -20,7 +21,7 @@ export const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
@@ -30,5 +31,5 @@ export const Router: VFC = memo(() => {
         <Page404 />
       </Route>
     </Switch>
-  )
-})
+  );
+});
