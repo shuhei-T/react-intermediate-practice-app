afb914a70b60b72f6aff201c6226987835c8ce26
shuhei-T <nofuture742617000027@gmail.com>
2022-06-16 21:16:45 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,33 @@
-import { memo, VFC } from 'react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/ransom"
-          userName="じゃけぇ"
-          fullName="Takumi Okada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/ransom"
+                userName={user.name}
+                fullName={user.username}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
